4c6f71064f3e2b3a6c7e4f0f6bcb76ed8b1a72a2
1021724152@qq.com <1021724152@qq.com>
2017-10-11 22:41:19 +0800

    modified four files B1

--- javagui/src/guiex/AMWGraph.java
+++ javagui/src/guiex/AMWGraph.java
@@ -8,7 +8,7 @@ public class AMWGraph {
 	public int numvertex;//存储点的个数
 	public int[][] edges;//邻接矩阵，用来存储边
 	public int numOfEdges;//边的数目
-        public int muk;
+        //public int muk;
 
     public AMWGraph() {
     	
--- javagui/src/guiex/GraphViz.java
+++ javagui/src/guiex/GraphViz.java
@@ -15,6 +15,7 @@ import java.util.Set;
 import java.util.Stack;
 
 public class GraphViz {
+        //public String cunth = "";
 	private String runPath = "";                      //文件的路径
 	private String dotPath = "";
 	private String runOrder = "";
